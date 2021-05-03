34220d06a20ee8679b8c9dfac7ed24d89a68a41e
Austin Spencer <abspencer2097@gmail.com>
2021-05-03 11:31:47 -0700

    Move code to components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,63 +1,20 @@
 <template>
   <v-container class="pa-5 mb-5 pb-20">
-    <v-row
-      style="padding-top: 30vh; padding-bottom: 50vh;"
-      justify="center"
-      align="center"
-    >
-      <v-col
-        sm="6"
-        md="4"
-        lg="4"
-      >
-        <span>Hello, my name is</span>
-        <h1 class="primary--text">Austin Spencer</h1>
-        <p>Thanks for visiting my website!</p>
-      </v-col>
-      <v-col
-        sm="2"
-        md="2"
-        lg="2"
-      >
-        <v-btn
-          outlined
-          color="primary"
-          href="mailto:abspencer2097@gmail.com"
-        >
-          Say Hi!
-        </v-btn>
-      </v-col>
-    </v-row>
-    <v-row
-      style="padding-bottom: 10vh;"
-      justify="center"
-      align="center"
-      class="about"
-    >
-      <v-col
-        sm="10"
-        md="8"
-        lg="6"
-      >
-        <h3>About Me</h3>
-        <hr>
-        <br>
-        <p>I am a Senior at Arizona State University expecting to graduate in May, 2022 with a Computer Science degree. I am working as a Data Engineer Intern for <code
-            @click="USAA()"
-            class="usaa primary--text"
-          >USAA</code> this summer. </p>
-        <p>I grew up in central Illinois
-          and moved to Arizona ~2 years ago. I originally went to school to study Biology but after a couple years, found my true passion in programming.
-        </p>
-        <p>When I'm not working or studying I like to hike and play sports. My newfound hobby that I enjoy a lot is snowboarding!</p>
-      </v-col>
-    </v-row>
+    <landing />
+    <about />
   </v-container>
 </template>
 
 <script>
+import Landing from "../components/Home/Landing.vue";
+import About from "../components/Home/About.vue";
+
 export default {
   name: "Home",
+  components: {
+    landing: Landing,
+    about: About,
+  },
   methods: {
     USAA() {
       location.href = "https://www.usaa.com/";
