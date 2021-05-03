a64f87833a8d81887fa1f2f3f3b9e8b1bb2efead
Austin Spencer <abspencer2097@gmail.com>
2021-05-03 12:30:44 -0700

    add work history

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,21 @@
   <v-container class="pa-5 mb-5 pb-20">
     <landing />
     <about />
+    <work-history />
   </v-container>
 </template>
 
 <script>
 import Landing from "../components/Home/Landing.vue";
 import About from "../components/Home/About.vue";
+import WorkHistory from "../components/Home/WorkHistory.vue";
 
 export default {
   name: "Home",
   components: {
     landing: Landing,
     about: About,
+    "work-history": WorkHistory,
   },
   methods: {
     USAA() {
