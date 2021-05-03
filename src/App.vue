649828ae7933f49ed9882515d0fcc171f3cf4286
Austin Spencer <abspencer2097@gmail.com>
2021-05-03 02:56:08 -0700

    add footer

--- src/App.vue
+++ src/App.vue
@@ -8,17 +8,19 @@
     <v-main>
       <router-view></router-view>
     </v-main>
-    <!-- <app-footer v-show="atAbout" /> -->
+    <app-footer v-show="atHome" />
   </v-app>
 </template>
 
 <script>
 import AppMenu from "./components/AppMenu.vue";
+import AppFooter from "./components/AppFooter.vue";
 
 export default {
   name: "App",
   components: {
     "app-menu": AppMenu,
+    "app-footer": AppFooter,
   },
   data: () => ({
     //
@@ -29,6 +31,9 @@ export default {
     },
     atAbout() {
       return this.$route.name === "About" ? true : false;
+    },
+    atHome() {
+      return this.$route.name === "Home" ? true : false;
     },
   },
 };
