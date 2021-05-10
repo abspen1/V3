0ab37436e6e306b3555b795d85ef242b1ff4ccb2
Austin Spencer <abspencer2097@gmail.com>
2021-05-10 08:57:53 -0400

    add the transition

--- src/components/AppMenu.vue
+++ src/components/AppMenu.vue
@@ -103,19 +103,21 @@
       </v-btn>
     </v-app-bar>
     <!-- Button that appears once you start scrolling on the page that will take you to the top -->
-    <v-btn
-      v-scroll="onScroll"
-      v-show="fab"
-      fab
-      dark
-      fixed
-      bottom
-      right
-      color="primary"
-      @click="toTop"
-    >
-      <v-icon>mdi-arrow-up-thick</v-icon>
-    </v-btn>
+    <v-fab-transition>
+      <v-btn
+        v-scroll="onScroll"
+        v-show="fab"
+        fab
+        dark
+        fixed
+        bottom
+        right
+        color="primary"
+        @click="toTop"
+      >
+        <v-icon>mdi-arrow-up-thick</v-icon>
+      </v-btn>
+    </v-fab-transition>
   </div>
 </template>
 
